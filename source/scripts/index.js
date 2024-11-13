f216ebf19ee77dfa2f940d1c44ea22b420c6a0bd
dasha-deriglazova <deriglazovadaria27@gmail.com>
2024-11-13 16:03:01 +0400

    Добавляет адаптацию декоративных элементов

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
 /* в этот файл добавляет скрипты*/
+const buttonMenu = document.querySelector('.button-toggle');
+const menu = document.querySelector('.main-nav__list');
+const nav = document.querySelector('.main-nav');
+nav.classList.remove('main-nav--no-js');
+
+
+buttonMenu.classList.remove('button-toggle--open');
+buttonMenu.classList.remove('button-toggle--no-js');
+
+buttonMenu.addEventListener('click', () => {
+  buttonMenu.classList.toggle('button-toggle--close');
+  menu.classList.toggle('main-nav__list--open');
+});
