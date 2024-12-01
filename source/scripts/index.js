7379cf7f6cc69c1731a27303692de069fcc08fff
dasha-deriglazova <deriglazovadaria27@gmail.com>
2024-12-01 18:27:27 +0400

    Исправляет ошибки в стилях и коде

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,14 +1,48 @@
 /* в этот файл добавляет скрипты*/
-const buttonMenu = document.querySelector('.button-toggle');
-const menu = document.querySelector('.main-nav__list');
-const nav = document.querySelector('.main-nav');
-nav.classList.remove('main-nav--no-js');
+const mainNav = document.querySelector('.main-nav');
+const burger = document.querySelector('.button-toggle');
+const body = document.querySelector('.page__body');
 
+body.classList.remove('nojs');
+mainNav.classList.remove('main-nav--opened');
+mainNav.classList.add('main-nav--closed');
 
-buttonMenu.classList.remove('button-toggle--open');
-buttonMenu.classList.remove('button-toggle--no-js');
-
-buttonMenu.addEventListener('click', () => {
-  buttonMenu.classList.toggle('button-toggle--close');
-  menu.classList.toggle('main-nav__list--open');
+burger.addEventListener('click', ()=> {
+  if (mainNav.classList.contains('main-nav--opened')) {
+    burger.classList.remove('button-toggle--opened');
+    burger.classList.add('button-toggle--closed');
+    mainNav.classList.remove('main-nav--opened');
+    mainNav.classList.add('main-nav--closed');
+  } else {
+    burger.classList.add('button-toggle--opened');
+    burger.classList.remove('button-toggle--closed');
+    mainNav.classList.add('main-nav--opened');
+    mainNav.classList.remove('main-nav--closed');
+  }
 });
+
+const slider = document.querySelector('.slider');
+const curtain = slider.querySelector('.slider__button-wrapper');
+const sliderStyles = getComputedStyle(slider);
+let curtainPlaceStart;
+let clientX;
+
+window.addEventListener('pointerup', stopTheCurtainShifting);
+curtain.addEventListener('pointerdown', startTheCurtainShifting);
+
+function startTheCurtainShifting (event) {
+  curtainPlaceStart = Number(sliderStyles.getPropertyValue('--curtain-place'));
+  clientX = event.clientX;
+  window.addEventListener('pointermove', shiftТheСurtain);
+}
+
+function shiftТheСurtain (event) {
+  const deltaX = event.clientX - clientX;
+  const cursorPlace = curtainPlaceStart + deltaX / slider.clientWidth;
+  const curtainPlace = Math.min(Math.max(cursorPlace, 0), 1);
+  slider.style.setProperty('--curtain-place', `${curtainPlace}`);
+}
+
+function stopTheCurtainShifting () {
+  window.removeEventListener('pointermove', shiftТheСurtain);
+}
